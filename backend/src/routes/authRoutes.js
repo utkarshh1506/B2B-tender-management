e8e3bf5c61c61815ab484c68a38b99518f791e37
utkarshh1506 <utksin9621@gmail.com>
2025-07-01 18:51:01 +0530

    /me route added

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const { signup, login } = require('../controllers/authControllers')
+const verifyToken = require('../middlewares/verifyToken')
 const router = express.Router()
+const pool = require('../config/db')
 
 
 router.post('/signup',signup)
 router.post('/login',login)
+router.get('/me',verifyToken,async(req,res)=>{
+    const userId = req.user.userId
+    try {
+       const result = await pool.query(
+        'SELECT id,email from users where id = $1',[userId]
+       ) 
+       const user = result.rows[0]
+       res.json({user})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            error:'Failed to fetch user info'
+        })        
+    }
+})
 
 module.exports = router
