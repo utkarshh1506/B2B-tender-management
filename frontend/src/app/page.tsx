c481138e7e940842b68f67edf430da4dc754fefe
utkarshh1506 <utksin9621@gmail.com>
2025-07-02 19:38:49 +0530

    frontend setup done

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-gray-800">
+      <h1 className="text-4xl font-bold">🚀 Welcome to your Tender Platform</h1>
+    </main>
+  )
+}
