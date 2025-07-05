17d49b8aa7240b1f51206272c61cba6b71084ea5
utkarshh1506 <utksin9621@gmail.com>
2025-07-05 12:11:21 +0530

    updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,36 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const authRoutes = require('./routes/authRoutes')
-const companyRoutes = require('./routes/companyRoutes')
-const companyLogoRoutes = require('./routes/companyRoutes')
-const tenderRoutes = require('./routes/tenderRoutes')
-const applicationRoutes = require('./routes/applicationRoutes')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const companyRoutes = require("./routes/companyRoutes");
+const companyLogoRoutes = require("./routes/companyRoutes");
+const tenderRoutes = require("./routes/tenderRoutes");
+const applicationRoutes = require("./routes/applicationRoutes");
 
-dotenv.config()
-
+dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: ['https://b2-b-tender-management-dykjgj9f6.vercel.app/'], 
-  credentials: true
-}));
-app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use('/api/auth', authRoutes)
-app.use('/api/companies',companyRoutes)
-app.use('/api/companies', companyLogoRoutes)
-app.use('/api/tender',tenderRoutes)
-app.use('/api/applications',applicationRoutes)
+// ✅ Updated CORS config
+app.use(
+  cors({
+    origin: [
+      "https://b2-b-tender-management-cpkroypy3.vercel.app",
+      "https://b2b-tender-management.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT} `)
-})
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/companies", companyRoutes);
+app.use("/api/companies", companyLogoRoutes);
+app.use("/api/tender", tenderRoutes);
+app.use("/api/applications", applicationRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
